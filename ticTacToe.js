698c18a708a5a20a10c0569e86bb832a279d1ff2
Sam Spencer <sspencerbtv@gmail.com>
2018-06-14 13:56:15 -0400

    successfully switching between player x and o

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,28 +1,38 @@
-let turnCount = 1
-let currentPlayer = 'X'
+
 
 let game = {
 
-    board: {
-        wireFrame : [1 , 2 , 3, 4 , 5, 6, 7, 8, 9]  
-    },
-    
-    
+    board: [1 , 2 , 3, 4 , 5, 6, 7, 8, 9],  
+    turnCount: 0,
+    currentPlayer: 'X'
 }
 
 function promptPlayer() {
     
-    console.log('Player ' + currentPlayer + ' choose a position')
+    console.log('Player ' + game.currentPlayer + ' choose a position')
     
     process.stdin.once('data', (userInput) => {
     
         let userValue = userInput.toString().trim();
 
+        let userIndex = userValue - 1
+
+        game.board[userIndex] = game.currentPlayer
+        game.turnCount++
+
+        if (game.currentPlayer === 'X') {
+            game.currentPlayer = 'O'
+        }
+        else {
+            game.currentPlayer = "X"
+        }
+
+        // console.log('userValue: ' + userValue)
+        // console.log('currentPlayer: ' + game.currentPlayer)
         
-        
-        // if (userValue === board.wireFrame[0]) {
-        //     let userValue = board.wireFrame[0];
-        //     return userValue.board.wireFrame(currentPlayer)
+        // if (userValue === board[0]) {
+        //     let userValue = board[0];
+        //     return userValue.board(currentPlayer)
         // }
 
         printBoard(game.board)
@@ -34,11 +44,11 @@ function promptPlayer() {
 
 function printBoard(board) {
 
-    console.log(` ${board.wireFrame[0]} | ${board.wireFrame[1]} | ${board.wireFrame[2]} `)
+    console.log(` ${board[0]} | ${board[1]} | ${board[2]} `)
     console.log(`------------`)
-    console.log(` ${board.wireFrame[3]} | ${board.wireFrame[4]} | ${board.wireFrame[5]} `)
+    console.log(` ${board[3]} | ${board[4]} | ${board[5]} `)
     console.log(`------------`)
-    console.log(` ${board.wireFrame[6]} | ${board.wireFrame[7]} | ${board.wireFrame[8]} `)
+    console.log(` ${board[6]} | ${board[7]} | ${board[8]} `)
 }
 
 
