66a7bc24d6bcf21fb22305020440199218431fe1
Sam Spencer <sspencerbtv@gmail.com>
2018-06-14 15:29:53 -0400

    add initial win statement

--- ticTacToe.js
+++ ticTacToe.js
@@ -16,7 +16,8 @@ function promptPlayer() {
         let userValue = parseInt(userInput.toString().trim());
 
         let userIndex = userValue - 1
-        console.log(userValue)
+
+        //console.log(userValue)
 
         //validation against user not putting in 1-9
 
@@ -32,34 +33,63 @@ function promptPlayer() {
             return promptPlayer()
         }
 
-        //
-
+        //Update game board with latest move
         game.board[userIndex] = game.currentPlayer
-        game.turnCount++
-
-        if (game.currentPlayer === 'X') {
-            game.currentPlayer = 'O'
-        }
-        else {
-            game.currentPlayer = "X"
-        }
-
-    
-
-        // console.log('userValue: ' + userValue)
-        // console.log('currentPlayer: ' + game.currentPlayer)
         
-        // if (userValue === board[0]) {
-        //     let userValue = board[0];
-        //     return userValue.board(currentPlayer)
-        // }
+        let gameHasBeenWon = detectWinState(game.board, game.currentPlayer)
+    
+      
+            // game.board[userIndex] = game.currentPlayer
 
-        printBoard(game.board)
-       promptPlayer()
+            if(gameHasBeenWon){
+
+                console.log(game.currentPlayer + ' wins!')
+                process.exit()
+            }
+            else {
+
+                if (game.currentPlayer === 'X') {
+                    game.currentPlayer = 'O'
+                }
+                else {
+                    game.currentPlayer = "X"
+                }
+            
+        
+                game.turnCount++
+    
+      
+                //determining if there is a draw
+    
+                if (game.turnCount === 9) {
+                    console.log('It is a Draw!!')
+                    process.exit()
+                }
+        
+                printBoard(game.board)
+                promptPlayer()
+
+        }
+
+
     
     });
 }
 
+ //determining if there is a win
+ //this function should interact with game.board and game.currentPlayer
+
+function detectWinState(board, currentPlayer){
+
+    // console.log(board[0] + ' ' + board[1] + ' ' + board[2] + ' -- ' + currentPlayer)
+    
+    if (board[0] === currentPlayer && board[1] === currentPlayer && board[2] === currentPlayer){
+        return true
+    }
+
+    return false
+
+}
 
 function printBoard(board) {
 
